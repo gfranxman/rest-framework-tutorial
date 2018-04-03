39ec48d040c855c35bcd8c9d81b30e11d41403eb
Glenn Franxman <gfranxman@gmail.com>
2018-04-03 17:05:00 -0400

    Mixin and base classes for DRF and cli, etc.

--- pizza_shop/models.py
+++ pizza_shop/models.py
@@ -1,7 +1,13 @@
-from django import db
 from django.db import models, transaction
 
 # demo static choices vs datamodel relationships
+from rest_framework.exceptions import ValidationError
+from rest_framework.utils import model_meta
+
+import logging
+
+
+logger = logging.getLogger(__file__)
 
 FOOD_TYPES=(
     ('any', 'Any'),   # not a meat or veg, but can be used on either veggie or meat lovers
@@ -30,16 +36,76 @@ class ToppingType(models.Model):
 
 class Topping(models.Model):
     name = models.CharField(unique=True, max_length=40)
-    type = models.CharField(choices=FOOD_TYPES, default=FOOD_TYPES[0][0], max_length=40)
+    type = models.CharField(choices=FOOD_TYPES, default=FOOD_TYPES[0][0],  max_length=40)
     topping_type = models.ForeignKey(ToppingType)
 
     def __str__(self):
         return self.name
 
+class ValidatedModel(models.Model):
+    class Meta:
+        abstract = True
 
-class Pizza(models.Model):
+    def validate(self):
+        self.full_clean()
+        # this can only validate existing objects because field.all uses the db
+        if hasattr(self, 'id') and self.id:
+            for m2m_field in self._meta.many_to_many:
+                attr = m2m_field.name
+                field = getattr(self, attr)
+                for val_func in m2m_field.validators:
+                    # validate each item
+                    for item in field.all():
+                        if not val_func(self, item):
+                            field.remove(item)
+                            logger.warn("removing item")
+
+
+    def save(self, *args, **kwargs):
+        self.validate()
+
+        return super(ValidatedModel, self).save(*args, **kwargs)
+
+
+def toppings_are_compatible(pizza, field, data=None, raise_exception=False):
+    """ verify the toppings are allowed for the pizza
+        return False or raise exception on first invalid topping.
+    """
+    attr_name = field.name
+    if data:
+        toppings = data.get(attr_name, [])
+    else:
+        toppings = pizza.toppings.all()
+
+    for topping in toppings:
+        if not pizza.pizza_type.is_compatible(topping):
+            if raise_exception:
+                raise ValidationError("cannot put {t} on {p} pizza".format(
+                    t=topping, p=pizza.pizza_type.name
+                ))
+            return False
+
+    return True
+
+
+def topping_is_compatible(pizza, topping, raise_exception=False):
+    """ verify the topping are allowed for the pizza
+        return False or raise exception on invalid topping.
+    """
+    if not pizza.pizza_type.is_compatible(topping):
+        if raise_exception:
+            raise ValidationError("cannot put {t} on {p} pizza".format(
+                t=topping, p=pizza.pizza_type.name
+            ))
+        return False
+
+    return True
+
+
+class Pizza(ValidatedModel):
     name = models.CharField(unique=True, max_length=40)
-    toppings = models.ManyToManyField(Topping, blank=True)
+    toppings = models.ManyToManyField(Topping, blank=True, validators=[topping_is_compatible,])  # , through='PizzaPizzaTopping')
+    # toppings = models.ManyToManyField(Topping, blank=True) #, through='PizzaTopping')
 
     # two ways to type the pizza: choices or foreign key
     pizza_type_char = models.CharField(choices=FOOD_TYPES, default=FOOD_TYPES[0][0], max_length=40)
@@ -48,28 +114,65 @@ class Pizza(models.Model):
     def __str__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        # import pdb; pdb.set_trace()
-        self.valid_toppings()
-        super(Pizza,self).save(*args, **kwargs)
+# attempts at replacing the related model manager fail because if we define
+# the model, then the APIs change and you have to have a separate api
+# for managin the relation ship.    If you tag the model as 'auto_created'
+# to fool it, the mirgration framework will remove your table.
+#
+# class PizzaTopping(models.Model):
+#     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+#     topping = models.ForeignKey(Topping, on_delete=models.CASCADE)
+#
+#     class Meta:
+#         auto_created = True
+#
+#     def save(self, *args, **kwargs):
+#         topping_is_compatible(self.pizza, self.topping)
+#         return super(PizzaTopping, self).save(*args, **kwargs)
+#
 
-    def valid_toppings(self):
-        # validate choices
-        # for topping in self.toppings.all():
-        #     if self.pizza_type_char in ('meat', 'veg'):
-        #         then all topping must either match, or be 'any'
-        #       if topping.type != self.pizza_type_char or topping.type != 'mixed':
-        #           raise db.IntegrityError(
-        #               "cannot put {t} on {p} pizza".format(
-        #                   t=topping, p=self.pizza_type_char
-        #               ))
 
-        # validate relations
-        if self.id:
-            for topping in self.toppings.all():
-                if not self.pizza_type.is_compatible(topping):
-                    raise db.IntegrityError("cannot put {t} on {p} pizza".format(
-                        t=topping, p=self.pizza_type.name
-                    ))
 
+# attempts to subclass the related mgr somehow loose the meta data con the class
+# def subclass_related_mgr(mgr):
+#     class ValMgr( mgr.__class__):
+#         def add(self, o):
+#             return super(ValMgr, self).add(o)
+#         def update(self, *args, **kwargs):
+#             return super(ValMgr, self).update( *args, **kwargs)
+#         def set(self, instance, values):
+#             return super(ValMgr, self).set(instance, values)
+#         pass
+#     return ValMgr(mgr)
+# Pizza.toppings.related_manager_cls = subclass_related_mgr(Pizza.toppings.related_manager_cls)
+# Pizza.toppings = subclass_related_mgr(Pizza.toppings)
+
+# def extend_instance(obj, cls):
+#     """Apply mixins to a class instance after creation"""
+#     base_cls = obj.__class__
+#     base_cls_name = obj.__class__.__name__
+#     obj.__class__ = type(base_cls_name, (cls, base_cls),{})
+
+
+# original_add = Pizza.toppings.related_manager_cls.add
+# ftype = type(original_add)
+#
+#def validated_add(mgr, *args):
+#    global original_add
+#
+#    # validate before adding to mgr.instance
+#    for a in args:
+#        if not topping_is_compatible(mgr.instance, a):
+#            raise ValueError("cant add {t} to {p}".format(t=a, p=mgr.instance))
+#    return original_add(mgr, *args)
+
+
+# Pizza.toppings.related_manager_cls.add = ftype(validated_add, Pizza.toppings.related_manager_cls, {})
+
+# class PizzaPizzaTopping(models.Model):
+#     pizza = models.ForeignKey(Pizza)
+#     topping = models.ForeignKey(Topping)
+#
+#     class Meta:
+#          db_table = 'pizza_shop_pizza_toppings'
 
--- pizza_shop/serializers.py
+++ pizza_shop/serializers.py
@@ -1,10 +1,26 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.relations import HyperlinkedIdentityField
 
-from .models import Pizza, Topping, ToppingType
+from .models import Pizza, Topping, ToppingType#, PizzaPizzaTopping
 
 
-class PizzaSerializer(serializers.HyperlinkedModelSerializer):
+class ValidateManyToManyFieldsMixin:
+    """ checks all validators on any ManyToManyFields for Models
+        this makes 'create' calls work just like updates, allowing the
+        ValidatedModel to appliy any validators on save during updates
+    """
+    def create(self, validated_data):
+        with transaction.atomic():
+            original_data = validated_data.copy()
+            instance = super(ValidateManyToManyFieldsMixin, self).create(validated_data)
+            # ^^ instance created without m2m's which are then added
+            #    then after they are added, we can call update with the original
+            # vv to re-validate
+            instance = super(ValidateManyToManyFieldsMixin, self).update(instance, original_data)
+        return instance
+
+class PizzaSerializer(ValidateManyToManyFieldsMixin, serializers.HyperlinkedModelSerializer):
     toppings = serializers.HyperlinkedRelatedField(
         many=True,
         view_name='topping-detail',
@@ -15,6 +31,7 @@ class PizzaSerializer(serializers.HyperlinkedModelSerializer):
         fields = '__all__'
         url = HyperlinkedIdentityField(view_name='pizza-detail',
                                        lookup_field='id')
+
 
 class ToppingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
